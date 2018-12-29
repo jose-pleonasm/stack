f4fe373a826548370f6c5e52edbdd9e5a0687570
Jose Pleonasm <jose.pleonasm@gmail.com>
2018-12-29 19:52:34 +0100

    refactoring: removed circular dependency

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { routes } from '../routes';
+import { home, news } from '../routes';
+import { Home } from './pages/Home';
+import { News } from './pages/News';
 import { NotFound } from './pages/NotFound';
 import '../../css/index.scss';
 
@@ -8,9 +10,8 @@ export const App = (props) => {
 	return (
 		<BrowserRouter>
 			<Switch>
-				{routes.map(route => (
-					<Route key={route.path} {...route} />
-				))}
+				<Route {...home} component={Home} />
+				<Route {...news} component={News} />
 				<Route component={NotFound} />
 			</Switch>
 		</BrowserRouter>
--- src/js/routes.js
+++ src/js/routes.js
@@ -1,17 +1,13 @@
-import { Home } from './components/pages/Home';
-import { News } from './components/pages/News';
 
 export const home = {
 	path: '/',
 	exact: true,
-	component: Home,
 	title: 'Home',
 };
 
 export const news = {
 	path: '/news',
 	exact: true,
-	component: News,
 	title: 'News',
 };
 
