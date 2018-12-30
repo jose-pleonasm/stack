22aaeb58ae0d84db78db44f68347001d21c90b20
Jose Pleonasm <jose.pleonasm@gmail.com>
2018-12-30 13:14:40 +0100

    react intl: added helmet

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { IntlProvider, addLocaleData } from 'react-intl';
 import cs from 'react-intl/locale-data/cs';
+import { Helmet } from 'react-helmet';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { LANG } from '../constants';
 import { home, news, article } from '../routes';
@@ -14,15 +15,18 @@ addLocaleData(cs);
 
 export const App = (props) => {
 	return (
-		<IntlProvider locale={LANG}>
-			<BrowserRouter>
-				<Switch>
-					<Route {...home} component={Home} />
-					<Route {...news} component={News} />
-					<Route {...article} component={Article} />
-					<Route component={NotFound} />
-				</Switch>
-			</BrowserRouter>
-		</IntlProvider>
+		<React.Fragment>
+			<Helmet htmlAttributes={{ lang: LANG }}/>
+			<IntlProvider locale={LANG}>
+				<BrowserRouter>
+					<Switch>
+						<Route {...home} component={Home} />
+						<Route {...news} component={News} />
+						<Route {...article} component={Article} />
+						<Route component={NotFound} />
+					</Switch>
+				</BrowserRouter>
+			</IntlProvider>
+		</React.Fragment>
 	);
 };
