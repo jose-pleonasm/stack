1398c77fe70b7b69800fde1430e778cd15142ac3
Jose Pleonasm <jose.pleonasm@gmail.com>
2019-01-05 12:34:01 +0100

    AppError export

--- src/js/core/index.js
+++ src/js/core/index.js
@@ -1,4 +1,5 @@
 export { createBrowserHistory } from 'history';
 export { App } from './App.js';
 export { createStore } from './redux.js';
+export { AppError } from './AppError.js';
 export { DynamicIntlProvider } from './DynamicIntlProvider.js';
